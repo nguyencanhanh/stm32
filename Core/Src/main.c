e4ebf27a54d71288b38d1d3807a05debdbb385bd
nguyencanhanh <nguyencanhanh148.com>
2023-09-23 15:03:21 +0700

    complete project 2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,93 +1,47 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
 #include "stdio.h"
-/* USER CODE END PTD */
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
 int checkFire, x;
+uint16_t ADC_ScanVal[4];
 
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
+/* Declare variable */
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
-
 TIM_HandleTypeDef htim1;
-
 UART_HandleTypeDef huart1;
 
-/* USER CODE BEGIN PV */
-uint16_t ADC_ScanVal[4];
-// Delay
-void Delay_us(uint16_t us)
-{
-	__HAL_TIM_SET_COUNTER(&htim1, 0); // TIM1->CNT = 0
-	while(__HAL_TIM_GET_COUNTER(&htim1) < us);
+/* Delay */
+void Delay_us(uint16_t us){
+	TIM1->CNT = 0;
+	while(TIM1->CNT < us);
 }
-void Delay_ms(uint16_t ms)
-{
-	while(ms)
-	{
+void Delay_ms(uint16_t ms){
+	while(ms){
 		Delay_us(1000);
 		ms--;
 	}
 }
-void on_voice(void)
-{
-	if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_6) == 0)
-	{
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 1);
-		Delay_ms(2000);
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 0);
-	}
 
+/* Enable Buzzer while button is pushed */
+void on_voice(void){
+	if(!(GPIOB->IDR & (1<<6))){
+		GPIOB->BSRR |= GPIO_PIN_14;     
+		Delay_ms(2000);
+		GPIOB->BRR |= GPIO_PIN_14;
+	}
 }
 
-// UART
-void UART_sendData(char* str)
-{
-	while(*str)
-	{
+/*  UART */
+void UART_sendData(char* str){
+	while(*str){
 		while((USART1->SR & (1<<7)) == 0);
 		USART1->DR = *str;
 		str++;
 	}
 	while((USART1->SR & (1<<6)) == 0);
 }
-void UART_sendFloat(float f)
-{
+
+void UART_sendFloat(float f){
 	char str[9];
 	int phanNguyen = (int)f;
 	int phanThapPhan = (int)((f-phanNguyen)*100);
@@ -95,38 +49,31 @@ void UART_sendFloat(float f)
 	UART_sendData(str);
 }
 
-//DHT22
-void DHT22_Start(void)
-{
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 0);
+/* DHT22 */
+void DHT22_Start(void){
+	GPIOB->BRR |= GPIO_PIN_5;
 	Delay_ms(20);
-	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 1);
+	GPIOB->BSRR |= GPIO_PIN_5;
 	TIM1->CNT = 0;
-	while(TIM1->CNT < 55)
-	{
-		if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_5) == 0)
+	while(TIM1->CNT < 55){
+		if(!(GPIOB->IDR & (1<<5)))
 			break;
 	}
 }
 
-uint8_t DHT22_checkResponse(void)
-{
+uint8_t DHT22_checkResponse(void){
 	uint8_t res = 0;
 	TIM1->CNT = 0;
-	while(TIM1->CNT < 90)
-	{
-		if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_5) && (TIM1->CNT > 70))
-		{
+	while(TIM1->CNT < 90){
+		if((GPIOB->IDR & (1<<5)) && (TIM1->CNT > 70)){
 			res++;
 			break;
 		}
 	}
 
 	TIM1->CNT = 0;
-	while(TIM1->CNT <95)
-	{
-		if(!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_5) && (TIM1->CNT > 70))
-		{
+	while(TIM1->CNT <95){
+		if(!(GPIOB->IDR & (1<<5)) && (TIM1->CNT > 70)){
 			res++;
 			break;
 		}
@@ -134,45 +81,33 @@ uint8_t DHT22_checkResponse(void)
 	return res;
 }
 
-void DHT22_readData(float *temp, float *hum)
-{
+void DHT22_readData(float *temp, float *hum){
 	uint8_t data[5];
-	uint8_t htim;
 	DHT22_Start();
 	uint8_t check = DHT22_checkResponse();
-	if(check == 2)
-	{
-		for(int i=0; i<5; i++)
-		{
-			for(int j=7; j>=0; j--)
-			{
+	if(check == 2){
+		for(int i = 0; i < 5; i++){
+			for(int j = 7; j >= 0; j--){
 				TIM1->CNT = 0;
-					while(TIM1->CNT < 60)
-					{
-						if(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_5) && (TIM1->CNT > 40))
-						{
-							break;
-						}
-					}
+        while(TIM1->CNT < 60){
+          if((GPIOB->IDR & (1<<5)) && (TIM1->CNT > 40)){
+            break;
+          }
+        }
 
-					TIM1->CNT = 0;
-					while(TIM1->CNT < 80)
-					{
-						if(!HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_5))
-						{
-							break;
-						}
-					}
-					htim = TIM1->CNT;
-					if(htim > 50)
-					{
-						data[i] |= (1<<j);
-					}
-					else
-						data[i] &= ~(1<<j);
+        TIM1->CNT = 0;
+        while(TIM1->CNT < 80){
+          if(!(GPIOB->IDR & (1<<5))){
+            break;
+          }
+        }
+        if(TIM1->CNT > 50){
+          data[i] |= (1<<j);
+        }
+        else
+          data[i] &= ~(1<<j);
 			}
 		}
-
 	}
 
 	*temp = ((float)(data[2]&0x7F)*256 + (float)data[3])/10.0;
@@ -180,75 +115,32 @@ void DHT22_readData(float *temp, float *hum)
 
 }
 
-void warning(uint16_t *ADC_ScanVal1, float temp, float hum, int *check)
-{
-	if(ADC_ScanVal1[0] > 1500  || ADC_ScanVal1[1] < 200 || ADC_ScanVal1[2] < 200 || ADC_ScanVal1[3] > 1500)
-	{
+void warning(uint16_t *ADC_ScanVal1, float temp, float hum, int *check){
+	if(ADC_ScanVal1[0] > 1500  || ADC_ScanVal1[1] < 200 || ADC_ScanVal1[2] < 200 || ADC_ScanVal1[3] > 1500){
 		*check = 1;
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 1);
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, 1);
-		UART_sendFloat(1);
-		UART_sendData(",");
-		UART_sendFloat(temp);
-		UART_sendData(",");
-		UART_sendFloat(hum);
-		UART_sendData("\n");
+		GPIOB->BSRR |= GPIO_PIN_14|GPIO_PIN_12;
+		printf("%d,%.2f,%.2f\n", 1, temp, hum); // @suppress("Float formatting support")
 	}
-	else
-	{
-		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_14, 0);
-		UART_sendFloat(0);
-		UART_sendData(",");
-		UART_sendFloat(temp);
-		UART_sendData(",");
-		UART_sendFloat(hum);
-		UART_sendData("\n");
+	else{
+		GPIOB->BRR |= GPIO_PIN_14;
+		printf("%d,%.2f,%.2f\n", 0, temp, hum); // @suppress("Float formatting support")
 	}
 }
-/* USER CODE END PV */
 
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_TIM1_Init(void);
 static void MX_USART1_UART_Init(void);
-/* USER CODE BEGIN PFP */
 
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
 	float temp=0, hum=0;
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
@@ -256,44 +148,33 @@ int main(void)
   MX_ADC1_Init();
   MX_TIM1_Init();
   MX_USART1_UART_Init();
-  /* USER CODE BEGIN 2 */
+
   HAL_TIM_Base_Start(&htim1);
   HAL_ADC_Start_DMA(&hadc1, (uint32_t*)ADC_ScanVal, 4);
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, 1);
-  /* USER CODE END 2 */
-  JUM:
+  GPIOB->BSRR |= GPIO_PIN_5;
+
+  JUMP:
   checkFire = 0;
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   while (1)
   {
-    /* USER CODE END WHILE */
-    /* USER CODE BEGIN 3 */
 	  on_voice();
 	  DHT22_readData(&temp, &hum);
 	  warning((uint16_t*)ADC_ScanVal, temp, hum, &checkFire);
+
 	  UART_sendFloat(temp);
 	  UART_sendData("\n");
-//	  Delay_ms(1000);
-//	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, 1);
-//	  HAL_Delay(1000);
-//	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, 0);
-	  HAL_Delay(1000);
+	  Delay_ms(1000);
+
 	  if(checkFire == 1)
 		  x++;
 	  if(x > 10 && ADC_ScanVal[0] < 1500 &&  ADC_ScanVal[1] > 200 && ADC_ScanVal[2] > 200 && ADC_ScanVal[3] < 1500 ){
-		  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, 0);
+		  GPIOB->BRR |= GPIO_PIN_12;
 		  x=0;
-		  goto JUM;
+		  goto JUMP;
 	  }
   }
-  /* USER CODE END 3 */
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -336,26 +217,11 @@ void SystemClock_Config(void)
   }
 }
 
-/**
-  * @brief ADC1 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_ADC1_Init(void)
 {
-
-  /* USER CODE BEGIN ADC1_Init 0 */
-
-  /* USER CODE END ADC1_Init 0 */
-
   ADC_ChannelConfTypeDef sConfig = {0};
 
-  /* USER CODE BEGIN ADC1_Init 1 */
-
-  /* USER CODE END ADC1_Init 1 */
-
-  /** Common config
-  */
+  /* Common config */
   hadc1.Instance = ADC1;
   hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
   hadc1.Init.ContinuousConvMode = ENABLE;
@@ -368,8 +234,7 @@ static void MX_ADC1_Init(void)
     Error_Handler();
   }
 
-  /** Configure Regular Channel
-  */
+  /* Configure Regular Channel */
   sConfig.Channel = ADC_CHANNEL_3;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_71CYCLES_5;
@@ -378,8 +243,7 @@ static void MX_ADC1_Init(void)
     Error_Handler();
   }
 
-  /** Configure Regular Channel
-  */
+  /* Configure Regular Channel */
   sConfig.Channel = ADC_CHANNEL_4;
   sConfig.Rank = ADC_REGULAR_RANK_2;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
@@ -387,8 +251,7 @@ static void MX_ADC1_Init(void)
     Error_Handler();
   }
 
-  /** Configure Regular Channel
-  */
+  /* Configure Regular Channel */
   sConfig.Channel = ADC_CHANNEL_5;
   sConfig.Rank = ADC_REGULAR_RANK_3;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
@@ -396,38 +259,19 @@ static void MX_ADC1_Init(void)
     Error_Handler();
   }
 
-  /** Configure Regular Channel
-  */
+  /* Configure Regular Channel */
   sConfig.Channel = ADC_CHANNEL_6;
   sConfig.Rank = ADC_REGULAR_RANK_4;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN ADC1_Init 2 */
-
-  /* USER CODE END ADC1_Init 2 */
-
 }
 
-/**
-  * @brief TIM1 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_TIM1_Init(void)
 {
-
-  /* USER CODE BEGIN TIM1_Init 0 */
-
-  /* USER CODE END TIM1_Init 0 */
-
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TIM1_Init 1 */
-
-  /* USER CODE END TIM1_Init 1 */
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 71;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -450,27 +294,10 @@ static void MX_TIM1_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN TIM1_Init 2 */
-
-  /* USER CODE END TIM1_Init 2 */
-
 }
 
-/**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_USART1_UART_Init(void)
 {
-
-  /* USER CODE BEGIN USART1_Init 0 */
-
-  /* USER CODE END USART1_Init 0 */
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
   huart1.Init.BaudRate = 115200;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
@@ -483,18 +310,11 @@ static void MX_USART1_UART_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART1_Init 2 */
-
-  /* USER CODE END USART1_Init 2 */
-
 }
 
-/**
-  * Enable DMA controller clock
-  */
+/* Enable DMA controller clock */
 static void MX_DMA_Init(void)
 {
-
   /* DMA controller clock enable */
   __HAL_RCC_DMA1_CLK_ENABLE();
 
@@ -504,12 +324,6 @@ static void MX_DMA_Init(void)
   HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 
 }
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -538,14 +352,6 @@ static void MX_GPIO_Init(void)
 
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -557,19 +363,20 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
+#ifdef __GNUC__
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+#else
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#endif
+
+PUTCHAR_PROTOTYPE
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, HAL_MAX_DELAY);
+  return ch;
+}
+
+#ifdef  USE_FULL_ASSERT
+
+void assert_failed(uint8_t *file, uint32_t line){
 }
 #endif /* USE_FULL_ASSERT */
